a92fae41733d64081aaec12c4d230cbb86da1a11
Heikki Kurhinen <heikki.kurhinen@gmail.com>
2016-04-23 22:50:32 +0300

    ** Changed model to include also happiness and contempt

--- model/measurement.js
+++ model/measurement.js
@@ -4,11 +4,12 @@ var Document = require('camo').Document;
 class Measurement extends Document {
   constructor() {
     super();
-
+    
     this.anger = Number;
+    this.contempt = Number;
     this.disgust = Number;
     this.fear = Number;
-    this.joy = Number;
+    this.happiness = Number;
     this.neutral = Number;
     this.sadness = Number;
     this.surprise = Number;
