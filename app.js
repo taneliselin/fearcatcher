6123330ccaf529c3b65f263764fd9d649bfa7687
Heikki Kurhinen <heikki.kurhinen@gmail.com>
2016-04-23 19:25:16 +0300

    ** Added basic database connections and websocket functionality

--- app.js
+++ app.js
@@ -1,7 +1,24 @@
 var express = require('express');
+var path = require('path');
+var connect = require('camo').connect;
+var Entry = require('./model/entry');
 var app = express();
+var database = 'nedb://./data';
+var currentScore = 0;
 
-var http = require('http').Server(app);
-http.liste(3000, function(){
-  console.log('server listening on port 3000');
+connect(database).then(function (db) {
+  app.set('views', path.join(__dirname, 'views'));
+  app.set('view engine', 'jade');
+  app.use(express.static(__dirname + '/public'));
+  
+  var http = require('http').Server(app);
+  var io = require('socket.io')(http); //TODO: set up websockets since we will surely need them later
+
+  app.get('/', function (req, res) {
+    res.render('index');
+  });
+
+  http.listen(3000, function () {
+    console.log('Express server listening on port 3000');
+  });
 });
