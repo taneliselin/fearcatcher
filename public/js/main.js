71ca00f991c6c8130806bc5bb3d19a1422c0c674
Heikki Kurhinen <heikki.kurhinen@gmail.com>
2016-04-24 11:49:48 +0300

    ** Added graphs

--- public/js/main.js
+++ public/js/main.js
@@ -1,24 +1,111 @@
-(function(){
+(function () {
   'use strict';
   var socket = io();
-  
-  socket.on('face:analyzed', function(data){
-    $('#anger').text(data.anger);
-    $('#contempt').text(data.contempt);
-    $('#disgust').text(data.disgust);
-    $('#fear').text(data.fear);
-    $('#happiness').text(data.happiness);
-    $('#neutral').text(data.neutral);
-    $('#sadness').text(data.sadness);
-    $('#surprise').text(data.surprise);
+
+  var max_data_points = 20;
+  var current_time = new Date().getTime();
+
+  var face_anger = [[current_time, 0]];
+  var face_contempt = [[current_time, 0]];
+  var face_disgust = [[current_time, 0]];
+  var face_fear = [[current_time, 0]];
+  var face_happiness = [[current_time, 0]];
+  var face_neutral = [[current_time, 0]];
+  var face_sadness = [[current_time, 0]];
+  var face_surprise = [[current_time, 0]];
+
+  var tone_anger = [[current_time, 0]];
+  var tone_disgust = [[current_time, 0]];
+  var tone_fear = [[current_time, 0]];
+  var tone_joy = [[current_time, 0]];
+  var tone_sadness = [[current_time, 0]];
+
+  var face_data = [
+    { data: face_anger, label: 'anger' },
+    { data: face_contempt, label: 'contempt' },
+    { data: face_disgust, label: 'disgust' },
+    { data: face_fear, label: 'fear' },
+    { data: face_happiness, label: 'happiness' },
+    { data: face_neutral, label: 'neutral' },
+    { data: face_sadness, label: 'sadness' },
+    { data: face_surprise, label: 'surprise' }
+  ];
+  var tone_data = [
+    { data: tone_anger, label: 'anger' },
+    { data: tone_disgust, label: 'disgust' },
+    { data: tone_fear, label: 'fear' },
+    { data: tone_joy, label: 'joy' },
+    { data: tone_sadness, label: 'sadness' }
+  ];
+
+  var face_plot = $.plot($('#face-container'), face_data, {
+    xaxis: { mode: 'time', show: false },
+    yaxis: {show: false}
   });
-  
-  socket.on('tone:analyzed', function(data){
-    $('#anger-tone').text(data.anger);
-    $('#disgust-tone').text(data.disgust);
-    $('#fear-tone').text(data.fear);
-    $('#joy-tone').text(data.joy);
-    $('#sadness-tone').text(data.sadness);
+  var tone_plot = $.plot($('#tone-container'), tone_data, {
+    xaxis: { mode: 'time', show: false },
+    yaxis: {show: false}
   });
-  
+
+  socket.on('face:analyzed', function (data) {
+    var new_time = new Date().getTime();
+    if (face_anger.length > max_data_points) {
+      face_anger.shift();
+      face_contempt.shift();
+      face_disgust.shift();
+      face_fear.shift();
+      face_happiness.shift();
+      face_neutral.shift();
+      face_sadness.shift();
+      face_surprise.shift();
+    }
+    face_anger.push([new_time, data.anger]);
+    face_contempt.push([new_time, data.contempt]);
+    face_disgust.push([new_time, data.disgust]);
+    face_fear.push([new_time, data.fear]);
+    face_happiness.push([new_time, data.happiness]);
+    face_neutral.push([new_time, data.neutral]);
+    face_sadness.push([new_time, data.sadness]);
+    face_surprise.push([new_time, data.surprise]);
+    var new_data = [
+      { data: face_anger, label: 'anger' },
+      { data: face_contempt, label: 'contempt' },
+      { data: face_disgust, label: 'disgust' },
+      { data: face_fear, label: 'fear' },
+      { data: face_happiness, label: 'happiness' },
+      { data: face_neutral, label: 'neutral' },
+      { data: face_sadness, label: 'sadness' },
+      { data: face_surprise, label: 'surprise' }
+    ];
+    face_plot.setData(new_data);
+    face_plot.setupGrid();
+    face_plot.draw();
+  });
+
+  socket.on('tone:analyzed', function (data) {
+    var new_time = new Date().getTime();
+    if (tone_anger.length > max_data_points) {
+      tone_anger.shift();
+      tone_disgust.shift();
+      tone_fear.shift();
+      tone_joy.shift();
+      tone_sadness.shift();
+    }
+    tone_anger.push([new_time, data.anger]);
+    tone_disgust.push([new_time, data.disgust]);
+    tone_fear.push([new_time, data.fear]);
+    tone_joy.push([new_time, data.joy]);
+    tone_sadness.push([new_time, data.sadness]);
+    var new_data = [
+      { data: tone_anger, label: 'anger' },
+      { data: tone_disgust, label: 'disgust' },
+      { data: tone_fear, label: 'fear' },
+      { data: tone_joy, label: 'joy' },
+      { data: tone_sadness, label: 'sadness' }
+    ];
+    tone_plot.setData(new_data);
+    tone_plot.setupGrid();
+    tone_plot.draw();
+  });
+
 })();
